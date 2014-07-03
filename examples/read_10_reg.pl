f647b156d59f0ae178b2e26b9c7ff51d01e29d71
Lefebvre Loïc <lle@cpan.org>
2014-07-03 14:26:04 +0000

    Import of LLE/MBclient-1.50 from CPAN.
    gitpan-cpan-distribution: MBclient
    gitpan-cpan-version:      1.50
    gitpan-cpan-path:         LLE/MBclient-1.50.tar.gz
    gitpan-cpan-author:       LLE
    gitpan-cpan-maturity:     released

---
+++ examples/read_10_reg.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+# for test MBclient perl module
+
+use strict;
+use MBclient;
+
+# create modbus object
+my $m = MBclient->new();
+
+# on local modbus server
+$m->host("127.0.0.1");
+$m->unit_id(1);
+# for print frame and debug string : uncomment this line
+#$m->{debug} = 1;
+
+# open TCP socket
+if (! $m->open()) {
+  print "unable to open TCP socket.\n";
+  exit(1);
+}
+
+# read register 0 to 9 and print it on stdout
+my $words = $m->read_holding_registers(0, 10);
+foreach my $word (@$words) {
+  print $word."\n";
+}
